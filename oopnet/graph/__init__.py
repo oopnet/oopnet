1f558242fec37f42b3f58842bc2295404cd105ed
Georg Arbesser-Rastburg <georg.arbesser-rastburg@tugraz.at>
2022-01-26 15:36:30 +0100

    refactoring
    - added MultiDiGraph to graph's module __init__.py

--- oopnet/graph/__init__.py
+++ oopnet/graph/__init__.py
@@ -1,1 +1,2 @@
-from .graph import Graph, DiGraph, MultiGraph, onlinks2nxlinks, nxlinks2onlinks, edgeresult2pandas, edge2pipeid
+from .graph import Graph, DiGraph, MultiGraph, MultiDiGraph, onlinks2nxlinks, nxlinks2onlinks, edgeresult2pandas, \
+    edge2pipeid
