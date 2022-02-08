4f5d9bde11134cf6a50b3bec1cd0439a3ae18544
Georg Arbesser-Rastburg <georg.arbesser-rastburg@tugraz.at>
2022-02-08 21:25:26 +0100

    refactor: refactored benchmark.py to incorporate new Network API

--- benchmark.py
+++ benchmark.py
@@ -4,9 +4,9 @@ from dataclasses import dataclass
 from typing import Optional
 
 from matplotlib import pyplot as plt
+import numpy as np
 
-from oopnet.elements.network import Network
-from oopnet.api import *
+import oopnet as on
 
 poulakis_filename = path.join('testing', 'networks', 'Poulakis_enhanced_PDA.inp')
 ctown_filename = path.join('examples', 'data', 'C-town.inp')
@@ -15,62 +15,62 @@ ctown_filename = path.join('examples', 'data', 'C-town.inp')
 @dataclass
 class OOPNETBenchmark:
     filename: str
-    network: Optional[Network] = None
+    network: Optional[on.Network] = None
 
     def __post_init__(self):
         self.reset()
 
     def reset(self):
-        self.network = Read(self.filename)
+        self.network = on.Network.read(self.filename)
 
     def read(self):
-        network = Read(self.filename)
+        network = on.Network.read(self.filename)
 
     def increase_demand(self):
-        for j in get_junctions(self.network):
+        for j in on.get_junctions(self.network):
             j.demand += 0.0001
 
     def change_length(self):
-        for p in get_pipes(self.network):
+        for p in on.get_pipes(self.network):
             p.length -= 0.0001
 
     def random_node_access(self):
         rng = np.random.default_rng(2021)
-        nids = get_node_ids(self.network)
+        nids = on.get_node_ids(self.network)
         rng.shuffle(nids)
 
         for nid in nids:
-            n = get_node(self.network, nid)
+            n = on.get_node(self.network, nid)
             n.xcoordinate, n.ycoordinate = (0, 0)
 
     def random_link_access(self):
         rng = np.random.default_rng(2021)
-        lids = get_link_ids(self.network)
+        lids = on.get_link_ids(self.network)
         rng.shuffle(lids)
 
         for lid in lids:
-            l = get_link(self.network, lid)
+            l = on.get_link(self.network, lid)
             l.status = 1
 
     def lookup_ids(self):
-        for node in get_nodes(self.network):
+        for node in on.get_nodes(self.network):
             id = node.id
 
-        for link in get_links(self.network):
+        for link in on.get_links(self.network):
             id = link.id
 
     def create_graph(self):
-        g = MultiGraph(self.network)
+        g = on.MultiGraph(self.network)
 
     def simulate(self):
-        rpt = Run(self.network)
+        rpt = self.network.run()
 
     def write(self):
-        Write(self.network, 'test.inp')
+        self.network.write('test.inp')
         remove('test.inp')
 
     def plot(self):
-        Plot(self.network)
+        self.network.plot()
         plt.close()
 
     def run_single_instance(self):
