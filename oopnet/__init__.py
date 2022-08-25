01453840fd249b5ebe37f935b3c5116c3630e346
Georg Arbesser <georg.arbesser-rastburg@tugraz.at>
2022-08-25 17:16:18 +0200

    docs: corrected Network attribute documentation

--- oopnet/__init__.py
+++ oopnet/__init__.py
@@ -4,6 +4,7 @@ from .elements import *
 from .graph import *
 from .plotter import *
 from .report import *
+from .simulator import *
 from .utils.adders import *
 from .utils.getters import *
 from .utils.removers import *
