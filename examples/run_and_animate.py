8bb6ceecc91002ff3c4773f4e331b0d666044e66
Georg <georg.arbesser-rastburg@tugraz.at>
2022-09-18 17:38:27 +0200

    docs: added an example for creating matplotlib animations

--- examples/run_and_animate.py
+++ examples/run_and_animate.py
@@ -1,22 +1,20 @@
 import os
-from datetime import timedelta
 
 from matplotlib import pyplot as plt
 from matplotlib.animation import PillowWriter
 
 import oopnet as on
-from oopnet.plotter.pyplot import NetworkPlotter
 
-filename = os.path.join('data', 'L-L-TOWN_AreaC.inp')
-
+filename = os.path.join('data', 'L-TOWN_AreaC.inp')
 net = on.Network.read(filename)
-net.times.reporttimestep = timedelta(hours=1)
+
 rpt = net.run()
 
-p = rpt.pressure
-f = rpt.flow
+p = rpt.pressure.loc['2016-01-01']
+f = rpt.flow.loc['2016-01-01']
+print(p)
+print(f)
 
-fig, ax = plt.subplots(figsize=(15, 10))
-anim = NetworkPlotter(robust=True).animate(net, ax=ax, nodes=p.abs(), links=f.abs(), node_label="Pressure (m)", link_label="Flow (m)", interval=300)
-anim.save("simple_animation.gif", dpi=300, writer=PillowWriter(fps=10))
-plt.show()
+fig, ax = plt.subplots(figsize=(7.5, 5))
+anim = net.animate(ax=ax, nodes=p, links=f.abs(), node_label="Pressure (m)", link_label="Flow (m)", robust=True, interval=50)
+anim.save("simple_animation.gif", dpi=200, writer=PillowWriter(fps=20))
