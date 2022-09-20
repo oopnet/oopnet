13a2c15d7f10a17b3de101928c8d88ad6ee04c99
Georg Arbesser <georg.arbesser-rastburg@tugraz.at>
2022-09-20 14:11:27 +0200

    [ci skip] fix bokeh plot in docs
    * docs: updated examples
    
    * docs: created user guide from old examples
    
    + fixed minor errors related to docstrings and type hints for the documentation
    + added some missing example script tests
    + added a documentation status badge to README.md
    + started revising about.rst
    
    * added missing extended_period_simulation.png
    
    * added plotting figures, fixed bokeh plot visualization in sphinx docs
    
    * fixed path to license file in README.md
    
    * added startdatetime argument passing in simulation example
    
    * some more doc updates, typo corrections, citations, ...
    
    * created dedicated bokeh plot script for docs

--- docs/bokehplot_userguide.py
+++ docs/bokehplot_userguide.py
@@ -1,5 +1,15 @@
 import os
-import sys
-os.chdir(os.path.join('..', 'examples'))
-import examples.bokeh_run_and_plot
-os.chdir(os.path.join('..', 'docs'))
+
+from bokeh.plotting import output_file, show
+import oopnet as on
+
+filename = os.path.join('..', 'examples', 'data', 'C-town.inp')
+net = on.Network.read(filename)
+
+rpt = net.run()
+p = rpt.pressure
+f = rpt.flow
+
+output_file('bokehexample.html')
+plot = net.bokehplot(nodes=p, links=f, colormap=dict(node='viridis', link='cool'))
+show(plot)
