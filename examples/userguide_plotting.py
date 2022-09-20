1e7d7bab82e767c6090a5ce1946c3a5bec684c66
georg <arbesser.georg@gmail.com>
2022-09-20 23:57:31 +0200

    started fixing colorbar positioning and sizing

--- examples/userguide_plotting.py
+++ examples/userguide_plotting.py
@@ -20,6 +20,6 @@ p_reduced = p.iloc[:50]
 net.plot(nodes=p_reduced, links=f, robust=True, truncate_nodes=True)
 
 diameters = on.get_diameter(net)
-net.plot(linkwidth=diameters, markersize=0)
+net.plot(linkwidth=diameters, links=f, markersize=0)
 
 plt.show()
