8fa7909e6f331eb33655c3581820539c31602995
Georg Arbesser <georg.arbesser-rastburg@tugraz.at>
2022-09-20 15:34:50 +0200

    [ci skip] rtd bugfix

--- docs/setup_environment.sh
+++ docs/setup_environment.sh
@@ -2,8 +2,9 @@
 git clone https://oopnet-bot:${GITHUB_TOKEN}@github.com/oopnet/linux_epanet_2.2.git
 cd linux_epanet_2.2 || exit
 make
-cur_path=$(pwd)
-echo "$cur_path"
-echo 'export PATH="$cur_path/epanet2:$PATH"' >> /home/docs/.bashrc
-source /home/docs/.bashrc
-echo "$PATH"
+#cur_path=$(pwd)
+#echo "$cur_path"
+#echo 'export PATH="$cur_path/epanet2:$PATH"' >> /home/docs/.bashrc
+#source /home/docs/.bashrc
+#echo "$PATH"
+cp epanet2 /home/docs/checkouts/readthedocs.org/user_builds/oopnet/envs/latest/bin/epanet2
